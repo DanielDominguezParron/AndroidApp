401de66b5b3e1f358ece2d7b76fbf0c051f949ac
Dani <daniel.parron@live.utad.com>
2019-10-16 14:19:34 +0200

    Load data on ConstraintLayout

---
+++ app/src/main/java/com/example/myapplication/Model/Movie.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.Model
+
+data class Movie(
+    var id: Int,
+    var title: String,
+    var year: String,
+    var genre: String,
+    var description: String,
+    var rating: Double
+)
